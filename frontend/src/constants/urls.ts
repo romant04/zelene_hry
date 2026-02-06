1805c527247988545926bedc7532819cf0b987a9
romant04 <roman.tarnai.04@gmail.com>
2026-02-06 16:21:44 +0100

    render

--- frontend/src/constants/urls.ts
+++ frontend/src/constants/urls.ts
@@ -1,2 +1,4 @@
-export const API = 'http://localhost:8080';
-export const WEBSOCKET = 'http://localhost:3000';
+import { env } from '$env/dynamic/public';
+
+export const API = env.PUBLIC_API_URL || 'http://localhost:8080';
+export const WEBSOCKET = env.PUBLIC_SOCKET_URL || 'http://localhost:3000';
