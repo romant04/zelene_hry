d517f67256c85198ad5479ab76ab6fa50e70aa8b
romant04 <roman.tarnai.04@gmail.com>
2026-02-06 16:32:37 +0100

    render

--- backend/src/main/java/com/tarnai/duelovky/configs/SecurityConfiguration.java
+++ backend/src/main/java/com/tarnai/duelovky/configs/SecurityConfiguration.java
@@ -54,7 +54,7 @@ public class SecurityConfiguration {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://backend-wbtv.onrender.com")); // Change to your frontend URL
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://frontend-17ax.onrender.com/")); // Change to your frontend URL
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true); // Allow credentials (important!)
